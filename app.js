6fb72d9a1aaa829d8c9ad4aca8c4ca1ef96de1d7
Mowaisrazaa <owaisr1292@gmail.com>
2025-06-09 18:07:59 +0500

    Add coin animation

--- app.js
+++ app.js
@@ -1,97 +1,40 @@
 
-// var body = document.querySelector("body");
-
-// function clickMe(){
-
-// body.style.backgroundColor = "red"
-// body.style.color = "white"
-
-// }
-// var input = document.querySelector("#input");
-// var cities = ["karachi", "larkana", "sakkhar", "nawabShah", "heydrabad", "sakrand"];
-
-// function checkcity(){
-//     console.log(input.value.toLowerCase())
-//     if(cities.includes(input.value.toLowerCase())){
-//         console.log("you are the live in safest city");
-//     } else{
-//         console.log("zindagi barbad")
-//     }
-
-// }
-
-// var userName = "Muhammad owais raza "
-
-// console.log(userName.concat("qadri attari "))
-
-// var sub = document.querySelector("#sub");
-
-
-
-
-
-
-
-
-
-//type of string
-
-// // string
-// var laptop = "DEll"
-
-
-// // number
-// var num = 10;
-
-
-// alert("my laptop brand is " + laptop)
-
-
-
-// legal varaibles Names
-
-// var name = "Ali";
-// var _username = "user123";
-// var $price = 100;
-// var myAge = 25;
-// var user1 = "Ahmed";
-// var camelCaseExample = true;
-
-
-
-
-// // illegal varaibles Names
-
-// var 1name = "Ali";       // ❌ Number se shuru
-// var my name = "Ahmed";   // ❌ Space allowed nahi
-// var var = "hello";       // ❌ Keyword use nahi kar sakte
-// var @price = 200;        // ❌ Special character not allowed
-// var user-name = "Sara";  // ❌ Hyphen not allowedvar
 
 
 
 var image = document.querySelector("#image");
-image.src = "https://en.numista.com/catalogue/photos/pakistan/424-original.jpg"
-var result = document.querySelector("#result")
+var result = document.querySelector("#result");
+image.src= "https://en.numista.com/catalogue/photos/pakistan/424-original.jpg"
 
-function toss(coin){
+function toss(coin) {
+    result.innerHTML = "Please Chose the toss Side ";
 
-    var randomNumber = Math.ceil(Math.random()*2)
-    console.log(randomNumber)
-    if(randomNumber === 1 && coin === "masjid" || coin === "chand" && randomNumber === 2){
+    // Start spin animation
+    image.classList.add("spin");
 
-        result.innerHTML = "you are win the toss"
-    } else {
-        result.innerHTML = "you are loss the toss"
-        
-    }
-    
-    if(randomNumber === 1){
-        image.src = "https://en.numista.com/catalogue/photos/pakistan/424-original.jpg"
-    } else {
-        image.src = "https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-18814.jpg"
-    }
+    setTimeout(() => {
+        // Remove spin animation
+        image.classList.remove("spin");
+
+        var randomNumber = Math.ceil(Math.random() * 2);
+        console.log(randomNumber);
+
+        if (randomNumber === 1) {
+            image.src = "https://en.numista.com/catalogue/photos/pakistan/424-original.jpg"; // Masjid side
+        } else {
+            image.src = "https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-18814.jpg"; // Chand side
+        }
+
+        // Show result
+        if ((randomNumber === 1 && coin === "masjid") || (randomNumber === 2 && coin === "chand")) {
+            result.innerHTML = "You won the Toss!";
+        } else {
+            result.innerHTML = "You lost the Toss.";
+        }
+
+    }, 2000); 
 }
+
 
 
 
